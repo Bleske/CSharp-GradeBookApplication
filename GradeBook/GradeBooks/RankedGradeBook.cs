e83ec5c36f8884add5964805ef5e66b650a9fdf0
Bleske <33684269+Bleske@users.noreply.github.com>
2019-01-21 13:00:51 +0100

    Task2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,53 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading reqiures a minimum of 5 students to work");
+            }
+
+            double over, under;
+            over = under = 1;
+
+            foreach (Student student in Students)
+            {
+                if (student.AverageGrade >= averageGrade)
+                {
+                    over += 1;
+                } else
+                {
+                    under += 1;
+                }
+            }
+
+            if (over == 0) {
+                return 'A';
+            }else if (under == 0){
+                return 'F';
+            }
+
+            int percent = (int)Math.Ceiling(over / over + under + 1);
+            if (percent > 80)
+            {
+                return 'A';
+            }else if (percent > 60)
+            {
+                return 'B';
+            }else if (percent > 40)
+            {
+                return 'C';
+            }else if (percent > 20)
+            {
+                return 'D';
+            } else
+            {
+                return 'F';
+            }
+
+
+        }
     }
 }
